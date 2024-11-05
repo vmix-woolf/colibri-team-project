d07760db679f42018c6c524d81d945532032af97
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-11-05 12:12:46 +0200

    add 3 methods in addressbook

--- assistant/addressbook.py
+++ assistant/addressbook.py
@@ -1,4 +1,12 @@
 from collections import UserDict
 
 class AddressBook(UserDict):
-    pass
+
+    def add_record(self, contact):
+        pass
+
+    def find_record(self, contact_name):
+        pass
+
+    def remove_record(self, contact_name):
+        pass
