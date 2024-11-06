8476ec6d7701693a7c2926942eb4c60736810515
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-11-06 11:20:49 +0200

    full set of methods for addresses

--- assistant/address.py
+++ assistant/address.py
@@ -3,7 +3,7 @@ from collections import UserDict
 
 class Address(UserDict):
 
-    def __init__(self, city, street, building, apartment=None):
+    def __init__(self, city, street, building, apartment):
         super().__init__()
         self.city = city
         self.street = street
