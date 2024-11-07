b5d18238e7263e21a14537577100120496783936
Atledd <artem.gonzha@gmail.com>
2024-11-07 21:18:34 +0200

    CCB-22-notes

--- handlers/handler.py
+++ handlers/handler.py
@@ -55,3 +55,58 @@ def edit_phone(args, book: AddressBook):
         return Constants.PHONE_UPDATED.value
     # TODO: разделить нужно эти две причины либо контакт не найден, либо номер не соответствует
     return "Contact not found or old phone number does not match."
+
+#Функціх для роботи із нотатками
+def add_note(manager):
+    # Додавання нотатки
+    title = input("Enter the title of the note: ")
+    content = input("Enter the content of the note: ")
+    manager.add_note(title, content)
+
+def search_note(manager):
+    # Пошук нотатки за текстом
+    text = input("Enter text to search for: ")
+    manager.search_text(text)
+
+def edit_note(manager, args):
+    # Редагування нотатки за ключем
+    try:
+        key = args[0]
+        title = input("Enter new title (leave empty to keep current): ")
+        content = input("Enter new content (leave empty to keep current): ")
+        manager.edit_note(int(key), title or None, content or None)
+    except IndexError:
+        print("Please provide a valid note key.")
+
+def remove_note(manager, args):
+    # Видалення нотатки за ключем
+    try:
+        key = int(args[0])
+        manager.remove_note(key)
+    except IndexError:
+        print("Please provide a valid note key.")
+
+def add_tag(manager, args):
+        # Додавання тегу до нотатки
+    if len(args) == 2:
+        title = args[0]
+        tag = args[1]
+        manager.add_tag_to_note(title, tag)
+    else:
+        print("Please provide title and tag.")
+
+def search_tag(manager, args):
+    # Пошук нотаток за тегом
+    tag = args[0]
+    manager.search_by_tag(tag)
+
+def sort_by_tag(manager, args):
+    # Сортування нотаток за тегом
+    try:
+        tag = args[0]
+        manager.sort_by_tag(tag)
+    except IndexError as e:
+        print(e) 
+
+def show_notes(manager):
+    manager.show_all_notes()
--- main.py
+++ main.py
@@ -7,12 +7,22 @@ from handlers.handler import (
     remove_phone,
     show_contacts,
     edit_phone,
-    add_phone
+    add_phone,
+    add_note,
+    search_note,
+    edit_note,
+    remove_note,
+    add_tag,
+    search_tag,
+    sort_by_tag,
+    show_notes
 )
+from assistant.notemanager import NoteManager
 
 def main():
     book = load_data()
     print(Constants.WELCOME_MESSAGE.value)
+    manager = NoteManager()
 
     while True:
         user_input = input("Enter a command: ")
@@ -36,6 +46,31 @@ def main():
             print(edit_phone(args, book))
         elif command == "all":
             show_contacts(book)
+        #Команди для роботи з нотатками
+        elif command == "add-note":
+            add_note(manager)
+
+        elif command == "search-text":
+            search_note(manager)
+
+        elif command == "edit-note":
+            edit_note(manager, args)
+
+        elif command == "remove-note":
+            remove_note(manager, args)
+
+        elif command == "add-tag":
+            add_tag(manager, args)
+
+        elif command == "search":
+            search_tag(manager, args)
+
+        elif command == "sort":
+            sort_by_tag(manager, args)
+
+        elif command == "show-notes":
+            show_notes(manager)
+
         else:
             print(Constants.INVALID_COMMAND_ERROR.value)
 
